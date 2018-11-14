b8a129887a784cd0e804e0b7275078ad33fcd8ac
David <dcarmona@empaquesnova.xom.mx>
2018-11-14 14:00:22 -0600

    new
    se crearon los archivos necesarios para que sea reconocido como módulo.
    se instalo cmo módulo vacio y completamente nuevo.

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'reportone',
+    'author': 'isc_dcm',
+    'website': 'https://www.empaquesnova.com.mx',
+    'category': 'Report',
+    'version': '1.0.0',
+    'depends': [
+    ],
+    'data': [
+        #'views/course.xml',
+    ],
+}
